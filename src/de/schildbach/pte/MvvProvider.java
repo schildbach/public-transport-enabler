8fb908bb0d069f07a294f97658fb6079e1529d67
andreas.schildbach <andreas.schildbach@0924bc21-9374-b0fa-ee44-9ff1593b38f0>
2010-07-19 23:23:32 +0000

    more lines
    git-svn-id: https://public-transport-enabler.googlecode.com/svn/trunk@14 0924bc21-9374-b0fa-ee44-9ff1593b38f0

--- src/de/schildbach/pte/MvvProvider.java
+++ src/de/schildbach/pte/MvvProvider.java
@@ -549,6 +549,8 @@ public class MvvProvider implements NetworkProvider
 				return "R" + line;
 			else if (line.startsWith("ICE "))
 				return "I" + line;
+			else if (line.startsWith("EC "))
+				return "I" + line;
 			else if (line.startsWith("CNL "))
 				return "I" + line;
 
