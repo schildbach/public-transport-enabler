8bea4d14b8ab27ca6bff3f6f09323ab684c0074b
andreas.schildbach <andreas.schildbach@0924bc21-9374-b0fa-ee44-9ff1593b38f0>
2010-11-06 20:49:15 +0000

    Ulm
    git-svn-id: https://public-transport-enabler.googlecode.com/svn/trunk@346 0924bc21-9374-b0fa-ee44-9ff1593b38f0

---
+++ test/de/schildbach/pte/live/DingProviderLiveTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2010 the original author or authors.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.schildbach.pte.live;
+
+import org.junit.Test;
+
+import de.schildbach.pte.DingProvider;
+import de.schildbach.pte.dto.NearbyStationsResult;
+import de.schildbach.pte.dto.QueryDeparturesResult;
+
+/**
+ * @author Andreas Schildbach
+ */
+public class DingProviderLiveTest
+{
+	private final DingProvider provider = new DingProvider();
+
+	@Test
+	public void nearbyStation() throws Exception
+	{
+		final NearbyStationsResult result = provider.nearbyStations("Ulm Hauptbahnhof", 0, 0, 0, 0);
+
+		System.out.println(result.stations.size() + "  " + result.stations);
+	}
+
+	@Test
+	public void queryDepartures() throws Exception
+	{
+		final QueryDeparturesResult result = provider.queryDepartures("Ulm", 0);
+
+		System.out.println(result.departures.size() + "  " + result.departures);
+	}
+}
