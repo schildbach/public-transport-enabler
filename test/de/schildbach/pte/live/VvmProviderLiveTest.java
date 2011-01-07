7a077f56c7a8bb99907da6bfa78b6d879b2ac995
andreas.schildbach@gmail.com <andreas.schildbach@gmail.com@0924bc21-9374-b0fa-ee44-9ff1593b38f0>
2011-01-07 23:18:33 +0000

    Nuremberg
    git-svn-id: https://public-transport-enabler.googlecode.com/svn/trunk@438 0924bc21-9374-b0fa-ee44-9ff1593b38f0

--- test/de/schildbach/pte/live/VvmProviderLiveTest.java
+++ test/de/schildbach/pte/live/VvmProviderLiveTest.java
@@ -15,40 +15,34 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package de.schildbach.pte;
+package de.schildbach.pte.live;
+
+import org.junit.Test;
+
+import de.schildbach.pte.VvmProvider;
+import de.schildbach.pte.dto.NearbyStationsResult;
+import de.schildbach.pte.dto.QueryDeparturesResult;
 
 /**
  * @author Andreas Schildbach
  */
-public enum NetworkId
+public class VvmProviderLiveTest
 {
-	// Germany
-	DB, BVG, RMV, MVV, INVG, AVV, VMV, SH, GVH, BSVAG, NASA, VVO, VMS, VGS, VRR, VRN, VVS, NALDO, DING, KVV, VVM,
+	private final VvmProvider provider = new VvmProvider();
 
-	// Austria
-	OEBB, VOR, LINZ, SVV, IVB, STV,
+	@Test
+	public void nearbyStation() throws Exception
+	{
+		final NearbyStationsResult result = provider.nearbyStations("Würzburg", 0, 0, 0, 0);
 
-	// Switzerland
-	SBB, BVB, VBL,
+		System.out.println(result.stations.size() + "  " + result.stations);
+	}
 
-	// Belgium
-	SNCB,
+	@Test
+	public void queryDepartures() throws Exception
+	{
+		final QueryDeparturesResult result = provider.queryDepartures("3000510", 0);
 
-	// Netherlands
-	NS,
-
-	// United Kingdom
-	TFL, TLEM, TLEA, TLSE, TLSW,
-
-	// Slovenia
-	MARIBOR,
-
-	// Italy
-	ATC,
-
-	// United Arab Emirates
-	DUB,
-
-	// United States
-	SF
+		System.out.println(result.departures.size() + "  " + result.departures);
+	}
 }
