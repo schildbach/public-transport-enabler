c9ff909871328d690f4f6e7bf2e3780b2f961005
andreas.schildbach@gmail.com <andreas.schildbach@gmail.com@0924bc21-9374-b0fa-ee44-9ff1593b38f0>
2011-02-02 20:14:16 +0000

    Ingolstadt
    git-svn-id: https://public-transport-enabler.googlecode.com/svn/trunk@479 0924bc21-9374-b0fa-ee44-9ff1593b38f0

---
+++ src/de/schildbach/pte/InvgProvider.java
@@ -0,0 +1,255 @@
+/*
+ * Copyright 2010 the original author or authors.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.schildbach.pte;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import de.schildbach.pte.dto.Departure;
+import de.schildbach.pte.dto.Location;
+import de.schildbach.pte.dto.LocationType;
+import de.schildbach.pte.dto.QueryDeparturesResult;
+import de.schildbach.pte.dto.QueryDeparturesResult.Status;
+import de.schildbach.pte.util.ParserUtils;
+
+/**
+ * @author Andreas Schildbach
+ */
+public class InvgProvider extends AbstractHafasProvider
+{
+	public static final NetworkId NETWORK_ID = NetworkId.INVG;
+	private static final String API_BASE = "http://fpa.invg.de/bin/";
+	private static final String API_URI = "http://fpa.invg.de/bin/extxml.exe";
+
+	private static final long PARSER_DAY_ROLLOVER_THRESHOLD_MS = 12 * 60 * 60 * 1000;
+
+	public InvgProvider()
+	{
+		super(API_URI, null);
+	}
+
+	public NetworkId id()
+	{
+		return NETWORK_ID;
+	}
+
+	public boolean hasCapabilities(final Capability... capabilities)
+	{
+		for (final Capability capability : capabilities)
+			if (capability == Capability.DEPARTURES)
+				return true;
+
+		return false;
+
+	}
+
+	private static final String[] PLACES = { "Ingolstadt", "München" };
+
+	@Override
+	protected String[] splitNameAndPlace(final String name)
+	{
+		for (final String place : PLACES)
+		{
+			if (name.startsWith(place + " ") || name.startsWith(place + "-"))
+				return new String[] { place, name.substring(place.length() + 1) };
+			else if (name.startsWith(place + ", "))
+				return new String[] { place, name.substring(place.length() + 2) };
+		}
+
+		return super.splitNameAndPlace(name);
+	}
+
+	private final String NEARBY_URI = API_BASE + "stboard.exe/dn?input=%s&distance=50&near=Anzeigen";
+
+	@Override
+	protected String nearbyStationUri(final String stationId)
+	{
+		return String.format(NEARBY_URI, ParserUtils.urlEncode(stationId));
+	}
+
+	@Override
+	protected char normalizeType(final String type)
+	{
+		final String ucType = type.toUpperCase();
+
+		if ("1".equals(ucType))
+			return 'B';
+
+		return 0;
+	}
+
+	private String departuresQueryUri(final String stationId, final int maxDepartures)
+	{
+		final DateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yy");
+		final DateFormat TIME_FORMAT = new SimpleDateFormat("HH:mm");
+		final Date now = new Date();
+
+		final StringBuilder uri = new StringBuilder();
+		uri.append(API_BASE).append("stboard.exe/dn");
+		uri.append("?input=").append(stationId);
+		uri.append("&boardType=dep");
+		uri.append("&time=").append(TIME_FORMAT.format(now));
+		uri.append("&date=").append(DATE_FORMAT.format(now));
+		uri.append("&productsFilter=1111111111");
+		if (maxDepartures != 0)
+			uri.append("&maxJourneys=").append(maxDepartures);
+		uri.append("&disableEquivs=yes"); // don't use nearby stations
+		uri.append("&start=yes");
+
+		return uri.toString();
+	}
+
+	private static final Pattern P_DEPARTURES_HEAD_COARSE = Pattern
+			.compile(
+					".*?" //
+							+ "(?:" //
+							+ "<div class=\"summary clearfix\">.*?<div class=\"block\">.*?(<div>.*?</div>.*?<div class=\"last\">.*?</div>).*?</div>.*?" //
+							+ "<div class=\"linkGroup\">.*?input=(\\d+).*?" // locationId
+							+ "(?:<table class=\"resultTable\" cellspacing=\"0\">(.*?)</table>|(verkehren an dieser Haltestelle keine))"//
+							+ "|(Eingabe kann nicht interpretiert)|(Verbindung zum Server konnte leider nicht hergestellt werden|kann vom Server derzeit leider nicht bearbeitet werden))" //
+							+ ".*?" //
+					, Pattern.DOTALL);
+	private static final Pattern P_DEPARTURES_HEAD_FINE = Pattern.compile(".*?" //
+			+ "<span class=\"output\">(.*?)<.*?" // location
+			+ "<span class=\"output\">\n(\\d{2}\\.\\d{2}\\.\\d{2}),\n" // date
+			+ "Abfahrt (\\d{1,2}:\\d{2}).*?" // time
+	, Pattern.DOTALL);
+	private static final Pattern P_DEPARTURES_COARSE = Pattern.compile("<tr class=\"(depboard-\\w*)\">(.*?)</tr>", Pattern.DOTALL);
+	private static final Pattern P_DEPARTURES_FINE = Pattern.compile(".*?" //
+			+ "<td class=\"time\">(\\d{1,2}:\\d{2})</td>\n" // plannedTime
+			+ "(?:<td class=\"[\\w ]*prognosis[\\w ]*\">\n" //
+			+ "(?:&nbsp;|<span class=\"rtLimit\\d\">(p&#252;nktlich|\\d{1,2}:\\d{2})</span>)\n</td>\n" // predictedTime
+			+ ")?.*?" //
+			+ "<img class=\"product\" src=\"/hafas-res/img/products/(\\w+)_pic\\.gif\"[^>]*>\\s*(.*?)\\s*</.*?" // type,
+																												// line
+			+ "<strong>\n" //
+			+ "<a href=\"http://fpa\\.invg\\.de/bin/stboard\\.exe/dn\\?input=(\\d+)&[^>]*>" // destinationId
+			+ "\\s*(.*?)\\s*</a>\n" // destination
+			+ "</strong>.*?" //
+			+ "(?:<td class=\"center sepline top\">\n(" + ParserUtils.P_PLATFORM + ").*?)?" // position
+	, Pattern.DOTALL);
+
+	public QueryDeparturesResult queryDepartures(final String stationId, final int maxDepartures) throws IOException
+	{
+		// scrape page
+		final String uri = departuresQueryUri(stationId, maxDepartures);
+		final CharSequence page = ParserUtils.scrape(uri);
+
+		// parse page
+		final Matcher mHeadCoarse = P_DEPARTURES_HEAD_COARSE.matcher(page);
+		if (mHeadCoarse.matches())
+		{
+			// messages
+			if (mHeadCoarse.group(4) != null)
+				return new QueryDeparturesResult(new Location(LocationType.STATION, Integer.parseInt(stationId)),
+						Collections.<Departure> emptyList(), null);
+			else if (mHeadCoarse.group(5) != null)
+				return new QueryDeparturesResult(Status.INVALID_STATION, Integer.parseInt(stationId));
+			else if (mHeadCoarse.group(6) != null)
+				return new QueryDeparturesResult(Status.SERVICE_DOWN, Integer.parseInt(stationId));
+
+			final int locationId = Integer.parseInt(mHeadCoarse.group(2));
+
+			final Matcher mHeadFine = P_DEPARTURES_HEAD_FINE.matcher(mHeadCoarse.group(1));
+			if (mHeadFine.matches())
+			{
+				final String location = ParserUtils.resolveEntities(mHeadFine.group(1));
+				final Date currentTime = ParserUtils.joinDateTime(ParserUtils.parseDate(mHeadFine.group(2)),
+						ParserUtils.parseTime(mHeadFine.group(3)));
+				final List<Departure> departures = new ArrayList<Departure>(8);
+				String oldZebra = null;
+
+				final Matcher mDepCoarse = P_DEPARTURES_COARSE.matcher(mHeadCoarse.group(3));
+				while (mDepCoarse.find())
+				{
+					final String zebra = mDepCoarse.group(1);
+					if (oldZebra != null && zebra.equals(oldZebra))
+						throw new IllegalArgumentException("missed row? last:" + zebra);
+					else
+						oldZebra = zebra;
+
+					final Matcher mDepFine = P_DEPARTURES_FINE.matcher(mDepCoarse.group(2));
+					if (mDepFine.matches())
+					{
+						final Calendar current = new GregorianCalendar();
+						current.setTime(currentTime);
+						final Calendar parsed = new GregorianCalendar();
+						parsed.setTime(ParserUtils.parseTime(mDepFine.group(1)));
+						parsed.set(Calendar.YEAR, current.get(Calendar.YEAR));
+						parsed.set(Calendar.MONTH, current.get(Calendar.MONTH));
+						parsed.set(Calendar.DAY_OF_MONTH, current.get(Calendar.DAY_OF_MONTH));
+						if (ParserUtils.timeDiff(parsed.getTime(), currentTime) < -PARSER_DAY_ROLLOVER_THRESHOLD_MS)
+							parsed.add(Calendar.DAY_OF_MONTH, 1);
+
+						final Date plannedTime = parsed.getTime();
+
+						Date predictedTime = null;
+						final String prognosis = ParserUtils.resolveEntities(mDepFine.group(2));
+						if (prognosis != null)
+						{
+							if (prognosis.equals("pünktlich"))
+								predictedTime = plannedTime;
+							else
+								predictedTime = ParserUtils.joinDateTime(currentTime, ParserUtils.parseTime(prognosis));
+						}
+
+						final String lineType = mDepFine.group(3);
+
+						final String line = normalizeLine(lineType, ParserUtils.resolveEntities(mDepFine.group(4)));
+
+						final int destinationId = mDepFine.group(5) != null ? Integer.parseInt(mDepFine.group(5)) : 0;
+
+						final String destination = ParserUtils.resolveEntities(mDepFine.group(6));
+
+						final String position = mDepFine.group(7) != null ? "Gl. " + ParserUtils.resolveEntities(mDepFine.group(7)) : null;
+
+						final Departure dep = new Departure(plannedTime, predictedTime, line, line != null ? lineColors(line) : null, null, position,
+								destinationId, destination, null);
+
+						if (!departures.contains(dep))
+							departures.add(dep);
+					}
+					else
+					{
+						throw new IllegalArgumentException("cannot parse '" + mDepCoarse.group(2) + "' on " + stationId);
+					}
+				}
+
+				final String[] nameAndPlace = splitNameAndPlace(location);
+				return new QueryDeparturesResult(new Location(LocationType.STATION, locationId, nameAndPlace[0], nameAndPlace[1]), departures, null);
+			}
+			else
+			{
+				throw new IllegalArgumentException("cannot parse '" + mHeadCoarse.group(1) + "' on " + stationId);
+			}
+		}
+		else
+		{
+			throw new IllegalArgumentException("cannot parse '" + page + "' on " + stationId);
+		}
+	}
+}
---
+++ test/de/schildbach/pte/live/InvgProviderLiveTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2010 the original author or authors.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.schildbach.pte.live;
+
+import org.junit.Test;
+
+import de.schildbach.pte.InvgProvider;
+import de.schildbach.pte.dto.NearbyStationsResult;
+import de.schildbach.pte.dto.QueryDeparturesResult;
+
+/**
+ * @author Andreas Schildbach
+ */
+public class InvgProviderLiveTest
+{
+	private final InvgProvider provider = new InvgProvider();
+
+	@Test
+	public void nearbyStation() throws Exception
+	{
+		final NearbyStationsResult result = provider.nearbyStations("80301", 0, 0, 0, 0);
+
+		System.out.println(result.stations.size() + "  " + result.stations);
+	}
+
+	@Test
+	public void queryDepartures() throws Exception
+	{
+		final QueryDeparturesResult result = provider.queryDepartures("80301", 0);
+
+		System.out.println(result.departures.size() + "  " + result.departures);
+	}
+}
