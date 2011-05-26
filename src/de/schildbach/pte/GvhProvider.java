618e2b9641437f8b61242eaa324267a309dfe238
andreas.schildbach@gmail.com <andreas.schildbach@gmail.com@0924bc21-9374-b0fa-ee44-9ff1593b38f0>
2011-05-26 12:38:29 +0000

    switched server address again, as requested by API operator
    git-svn-id: https://public-transport-enabler.googlecode.com/svn/trunk@661 0924bc21-9374-b0fa-ee44-9ff1593b38f0

--- src/de/schildbach/pte/GvhProvider.java
+++ src/de/schildbach/pte/GvhProvider.java
@@ -33,7 +33,7 @@ public class GvhProvider extends AbstractEfaProvider
 {
 	public static final NetworkId NETWORK_ID = NetworkId.GVH;
 	public static final String OLD_NETWORK_ID = "mobil.gvh.de";
-	private static final String API_BASE = "http://mobil.efa.de/mobile3/";
+	private static final String API_BASE = "http://mobil.gvh.de/mobile3/";
 
 	public GvhProvider(final String additionalQueryParameter)
 	{
