dc1f35a23fd99329045ce52cfe032bb545077564
andreas.schildbach@gmail.com <andreas.schildbach@gmail.com@0924bc21-9374-b0fa-ee44-9ff1593b38f0>
2011-10-21 18:35:15 +0000

    separate line and type
    git-svn-id: https://public-transport-enabler.googlecode.com/svn/trunk@833 0924bc21-9374-b0fa-ee44-9ff1593b38f0

--- src/de/schildbach/pte/VbnProvider.java
+++ src/de/schildbach/pte/VbnProvider.java
@@ -20,6 +20,7 @@ package de.schildbach.pte;
 import java.io.IOException;
 import java.util.List;
 
+import de.schildbach.pte.dto.Line;
 import de.schildbach.pte.dto.Location;
 import de.schildbach.pte.dto.LocationType;
 import de.schildbach.pte.dto.NearbyStationsResult;
@@ -156,6 +157,12 @@ public class VbnProvider extends AbstractHafasProvider
 		final String uri = String.format(AUTOCOMPLETE_URI, ParserUtils.urlEncode(constraint.toString(), ENCODING));
 
 		return jsonGetStops(uri);
+	}
+
+	@Override
+	protected Line normalizeLine(final String line)
+	{
+		return parseLineAndType(line);
 	}
 
 	@Override
