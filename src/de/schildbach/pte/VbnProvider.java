8581e402c418f3a7964780e40d8d4678cb9915f2
andreas.schildbach@gmail.com <andreas.schildbach@gmail.com@0924bc21-9374-b0fa-ee44-9ff1593b38f0>
2012-02-04 19:12:45 +0000

    disabled directions on Bremen, Bremerhaven & Oldenburg
    git-svn-id: https://public-transport-enabler.googlecode.com/svn/trunk@932 0924bc21-9374-b0fa-ee44-9ff1593b38f0

--- src/de/schildbach/pte/VbnProvider.java
+++ src/de/schildbach/pte/VbnProvider.java
@@ -47,7 +47,7 @@ public class VbnProvider extends AbstractHafasProvider
 	public boolean hasCapabilities(Capability... capabilities)
 	{
 		for (final Capability capability : capabilities)
-			if (capability == Capability.AUTOCOMPLETE_ONE_LINE || capability == Capability.DEPARTURES || capability == Capability.CONNECTIONS)
+			if (capability == Capability.AUTOCOMPLETE_ONE_LINE || capability == Capability.DEPARTURES)
 				return true;
 
 		return false;
