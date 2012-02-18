c978123e5211d7ae4b917adee0f9e001fab4dfc2
Andreas Schildbach <andreas@schildbach.de>
2012-02-18 22:13:32 +0100

    extract connection to own controller

--- src/main/java/de/schildbach/pte/service/ConnectionController.java
+++ src/main/java/de/schildbach/pte/service/ConnectionController.java
@@ -18,7 +18,7 @@
 package de.schildbach.pte.service;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.Date;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,31 +26,33 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import de.schildbach.pte.NetworkProvider.Accessibility;
+import de.schildbach.pte.NetworkProvider.WalkSpeed;
 import de.schildbach.pte.RtProvider;
 import de.schildbach.pte.dto.Location;
 import de.schildbach.pte.dto.LocationType;
-import de.schildbach.pte.dto.NearbyStationsResult;
+import de.schildbach.pte.dto.QueryConnectionsResult;
 
 /**
  * @author Andreas Schildbach
  */
 @Controller
-public class LocationController
+public class ConnectionController
 {
 	private final RtProvider provider = new RtProvider();
 
-	@RequestMapping(value = "/location/suggest", method = RequestMethod.GET)
+	@RequestMapping(value = "/connection", method = RequestMethod.GET)
 	@ResponseBody
-	public List<Location> autocomplete(@RequestParam("q") final String query) throws IOException
+	public QueryConnectionsResult connection(@RequestParam(value = "fromType", required = false, defaultValue = "ANY") final LocationType fromType,
+			@RequestParam(value = "from", required = false) final String from,
+			@RequestParam(value = "fromId", required = false, defaultValue = "0") final int fromId,
+			@RequestParam(value = "toType", required = false, defaultValue = "ANY") final LocationType toType,
+			@RequestParam(value = "to", required = false) final String to,
+			@RequestParam(value = "toId", required = false, defaultValue = "0") final int toId) throws IOException
 	{
-		return provider.autocompleteStations(query);
-	}
-
-	@RequestMapping(value = "/location/nearby", method = RequestMethod.GET)
-	@ResponseBody
-	public NearbyStationsResult nearby(@RequestParam("lat") final int lat, @RequestParam("lon") final int lon) throws IOException
-	{
-		final Location location = new Location(LocationType.ANY, lat, lon);
-		return provider.queryNearbyStations(location, 5000, 100);
+		final Location fromLocation = new Location(fromType, fromId, null, from);
+		final Location toLocation = new Location(toType, toId, null, to);
+		final String products = "IRSUTBFC";
+		return provider.queryConnections(fromLocation, null, toLocation, new Date(), true, products, WalkSpeed.NORMAL, Accessibility.NEUTRAL);
 	}
 }
