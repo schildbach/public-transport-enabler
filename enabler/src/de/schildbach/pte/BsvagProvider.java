b9c385e3116bf02181dfbb9e02a7a466d71a363c
Andreas Schildbach <andreas@schildbach.de>
2012-08-12 17:13:13 +0200

    line styles of Braunschweig

--- enabler/src/de/schildbach/pte/BsvagProvider.java
+++ enabler/src/de/schildbach/pte/BsvagProvider.java
@@ -18,10 +18,13 @@
 package de.schildbach.pte;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import de.schildbach.pte.dto.Location;
 import de.schildbach.pte.dto.LocationType;
+import de.schildbach.pte.dto.Style;
 
 /**
  * @author Andreas Schildbach
@@ -55,4 +58,65 @@ public class BsvagProvider extends AbstractEfaProvider
 	{
 		return xmlStopfinderRequest(new Location(LocationType.STATION, 0, null, constraint.toString()));
 	}
+
+	private static final Map<String, Style> LINES = new HashMap<String, Style>();
+
+	static
+	{
+		// Braunschweig
+		LINES.put("TM1", new Style(Style.parseColor("#62c2a2"), Style.WHITE));
+		LINES.put("TM2", new Style(Style.parseColor("#b35e89"), Style.WHITE));
+		LINES.put("TM3", new Style(Style.parseColor("#f9b5b9"), Style.WHITE));
+		LINES.put("TM4", new Style(Style.parseColor("#811114"), Style.WHITE));
+		LINES.put("TM5", new Style(Style.parseColor("#ffd00b"), Style.WHITE));
+
+		LINES.put("BM11", new Style(Style.parseColor("#88891e"), Style.WHITE));
+		LINES.put("BM13", new Style(Style.parseColor("#24a06d"), Style.WHITE));
+		LINES.put("BM16", new Style(Style.parseColor("#f8991b"), Style.WHITE));
+		LINES.put("BM19", new Style(Style.parseColor("#2c2768"), Style.WHITE));
+		LINES.put("BM29", new Style(Style.parseColor("#2c2768"), Style.WHITE));
+
+		LINES.put("B412", new Style(Style.parseColor("#094f34"), Style.WHITE));
+		LINES.put("B414", new Style(Style.parseColor("#00bce4"), Style.WHITE));
+		LINES.put("B415", new Style(Style.parseColor("#b82837"), Style.WHITE));
+		LINES.put("B417", new Style(Style.parseColor("#2a2768"), Style.WHITE));
+		LINES.put("B418", new Style(Style.parseColor("#c12056"), Style.WHITE));
+		LINES.put("B420", new Style(Style.parseColor("#b7d55b"), Style.WHITE));
+		LINES.put("B422", new Style(Style.parseColor("#16bce4"), Style.WHITE));
+		LINES.put("B424", new Style(Style.parseColor("#ffdf65"), Style.WHITE));
+		LINES.put("B427", new Style(Style.parseColor("#b5d55b"), Style.WHITE));
+		LINES.put("B431", new Style(Style.parseColor("#fddb62"), Style.WHITE));
+		LINES.put("B433", new Style(Style.parseColor("#ed0e65"), Style.WHITE));
+		LINES.put("B434", new Style(Style.parseColor("#bf2555"), Style.WHITE));
+		LINES.put("B436", new Style(Style.parseColor("#0080a2"), Style.WHITE));
+		LINES.put("B437", new Style(Style.parseColor("#fdd11a"), Style.WHITE));
+		LINES.put("B442", new Style(Style.parseColor("#cc3f68"), Style.WHITE));
+		LINES.put("B443", new Style(Style.parseColor("#405a80"), Style.WHITE));
+		LINES.put("B445", new Style(Style.parseColor("#3ca14a"), Style.WHITE));
+		LINES.put("B450", new Style(Style.parseColor("#f2635a"), Style.WHITE));
+		LINES.put("B451", new Style(Style.parseColor("#f5791e"), Style.WHITE));
+		LINES.put("B452", new Style(Style.parseColor("#f0a3ca"), Style.WHITE));
+		LINES.put("B455", new Style(Style.parseColor("#395f95"), Style.WHITE));
+		LINES.put("B461", new Style(Style.parseColor("#00b8a0"), Style.WHITE));
+		LINES.put("B464", new Style(Style.parseColor("#00a14b"), Style.WHITE));
+		LINES.put("B465", new Style(Style.parseColor("#77234b"), Style.WHITE));
+		LINES.put("B471", new Style(Style.parseColor("#380559"), Style.WHITE));
+		LINES.put("B480", new Style(Style.parseColor("#2c2768"), Style.WHITE));
+		LINES.put("B481", new Style(Style.parseColor("#007ec1"), Style.WHITE));
+		LINES.put("B484", new Style(Style.parseColor("#dc8998"), Style.WHITE));
+		LINES.put("B485", new Style(Style.parseColor("#ea8d52"), Style.WHITE));
+		LINES.put("B493", new Style(Style.parseColor("#f24825"), Style.WHITE));
+		LINES.put("B560", new Style(Style.parseColor("#9f6fb0"), Style.WHITE));
+	}
+
+	@Override
+	public Style lineStyle(final String line)
+	{
+		final Style style = LINES.get(line);
+		if (style != null)
+			return style;
+		else
+			return super.lineStyle(line);
+	}
+
 }
