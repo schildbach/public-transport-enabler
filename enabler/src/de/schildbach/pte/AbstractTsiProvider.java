f7d14cad5eddab4e86f69d2c51d3b2b99a90a1ea
Kjell Braden <afflux@pentabarf.de>
2014-02-18 17:02:06 +0100

    add license

--- enabler/src/de/schildbach/pte/AbstractTsiProvider.java
+++ enabler/src/de/schildbach/pte/AbstractTsiProvider.java
@@ -1,3 +1,19 @@
+/*
+* Copyright 2014 Kjell Braden <afflux@pentabarf.de>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 package de.schildbach.pte;
 
 import java.io.IOException;
@@ -36,6 +52,9 @@ import de.schildbach.pte.dto.Trip;
 import de.schildbach.pte.exception.ParserException;
 import de.schildbach.pte.util.ParserUtils;
 
+/**
+* @author Kjell Braden <afflux@pentabarf.de>
+*/
 public abstract class AbstractTsiProvider extends AbstractNetworkProvider
 {
 	private static class Context implements QueryTripsContext
--- enabler/test/de/schildbach/pte/live/PacaProviderLiveTest.java
+++ enabler/test/de/schildbach/pte/live/PacaProviderLiveTest.java
@@ -1,3 +1,19 @@
+/*
+* Copyright 2014 Kjell Braden <afflux@pentabarf.de>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 package de.schildbach.pte.live;
 
 import static org.junit.Assert.assertEquals;
@@ -16,6 +32,9 @@ import de.schildbach.pte.dto.LocationType;
 import de.schildbach.pte.dto.Product;
 import de.schildbach.pte.dto.QueryTripsResult;
 
+/**
+* @author Kjell Braden <afflux@pentabarf.de>
+*/
 public class PacaProviderLiveTest extends AbstractProviderLiveTest
 {
 	public PacaProviderLiveTest()
