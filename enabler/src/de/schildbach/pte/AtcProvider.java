6fd0fdff4dcd2d32c55ec94158ad7d17c885b4a3
Andreas Schildbach <andreas@schildbach.de>
2014-07-16 12:08:20 +0200

    Migrate Bologna to JSON stopfinder.

--- enabler/src/de/schildbach/pte/AtcProvider.java
+++ enabler/src/de/schildbach/pte/AtcProvider.java
@@ -17,12 +17,6 @@
 
 package de.schildbach.pte;
 
-import java.io.IOException;
-
-import de.schildbach.pte.dto.Location;
-import de.schildbach.pte.dto.LocationType;
-import de.schildbach.pte.dto.SuggestLocationsResult;
-
 /**
  * @author Andreas Schildbach
  */
@@ -48,11 +42,5 @@ public class AtcProvider extends AbstractEfaProvider
 				return true;
 
 		return false;
-	}
-
-	@Override
-	public SuggestLocationsResult suggestLocations(final CharSequence constraint) throws IOException
-	{
-		return xmlStopfinderRequest(new Location(LocationType.STATION, null, null, constraint.toString()));
 	}
 }
