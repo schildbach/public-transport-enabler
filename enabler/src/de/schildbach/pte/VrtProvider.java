04abe7b7a7bae9786f43a5527d1de9d961b278f2
Andreas Schildbach <andreas@schildbach.de>
2014-07-16 12:19:54 +0200

    Migrate Trier to JSON stopfinder.

--- enabler/src/de/schildbach/pte/VrtProvider.java
+++ enabler/src/de/schildbach/pte/VrtProvider.java
@@ -17,12 +17,6 @@
 
 package de.schildbach.pte;
 
-import java.io.IOException;
-
-import de.schildbach.pte.dto.Location;
-import de.schildbach.pte.dto.LocationType;
-import de.schildbach.pte.dto.SuggestLocationsResult;
-
 /**
  * @author Andreas Schildbach
  */
@@ -48,11 +42,5 @@ public class VrtProvider extends AbstractEfaProvider
 				return true;
 
 		return false;
-	}
-
-	@Override
-	public SuggestLocationsResult suggestLocations(final CharSequence constraint) throws IOException
-	{
-		return xmlStopfinderRequest(new Location(LocationType.ANY, null, null, constraint.toString()));
 	}
 }
