ea4b5441e76a0d307dcb7382a832ae8046fde822
Andreas Schildbach <andreas@schildbach.de>
2014-07-20 10:50:18 +0200

    Add 'Zentrum' to names that need places in their unique form.

--- enabler/src/de/schildbach/pte/dto/Location.java
+++ enabler/src/de/schildbach/pte/dto/Location.java
@@ -105,8 +105,8 @@ public final class Location implements Serializable
 		return false;
 	}
 
-	private static final String[] NON_UNIQUE_NAMES = { "Hauptbahnhof", "Hbf", "Bahnhof", "Bf", "Busbahnhof", "ZOB", "Dorf", "Kirche", "Nord", "Ost",
-			"Süd", "West" };
+	private static final String[] NON_UNIQUE_NAMES = { "Hauptbahnhof", "Hbf", "Bahnhof", "Bf", "Busbahnhof", "ZOB", "Zentrum", "Dorf", "Kirche",
+			"Nord", "Ost", "Süd", "West" };
 	static
 	{
 		Arrays.sort(NON_UNIQUE_NAMES);
