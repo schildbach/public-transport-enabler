0517f99a4ebf680a302b43ae32d9d21c590d8c3f
Andreas Schildbach <andreas@schildbach.de>
2014-07-29 02:05:49 +0200

    Fix test cases for Kiel, Lübeck & Schleswig-Holstein.

--- enabler/test/de/schildbach/pte/live/ShProviderLiveTest.java
+++ enabler/test/de/schildbach/pte/live/ShProviderLiveTest.java
@@ -47,15 +47,7 @@ public class ShProviderLiveTest extends AbstractProviderLiveTest
 	@Test
 	public void nearbyStations() throws Exception
 	{
-		final NearbyStationsResult result = provider.queryNearbyStations(new Location(LocationType.STATION, "715210"), 0, 0);
-
-		print(result);
-	}
-
-	@Test
-	public void nearbyStationsByCoordinate() throws Exception
-	{
-		final NearbyStationsResult result = provider.queryNearbyStations(new Location(LocationType.ADDRESS, 54318356, 10130053), 0, 0);
+		final NearbyStationsResult result = provider.queryNearbyStations(new Location(LocationType.STATION, "8002547"), 0, 0);
 
 		print(result);
 	}
@@ -63,7 +55,7 @@ public class ShProviderLiveTest extends AbstractProviderLiveTest
 	@Test
 	public void queryDepartures() throws Exception
 	{
-		final QueryDeparturesResult result = provider.queryDepartures("715210", 0, false);
+		final QueryDeparturesResult result = provider.queryDepartures("8002547", 0, false);
 
 		print(result);
 	}
@@ -104,7 +96,7 @@ public class ShProviderLiveTest extends AbstractProviderLiveTest
 	public void shortTrip() throws Exception
 	{
 		final QueryTripsResult result = queryTrips(new Location(LocationType.STATION, "8002547", null, "Flughafen Hamburg"), null, new Location(
-				LocationType.STATION, "715210", null, "Flughafen, Lübeck"), new Date(), true, Product.ALL, WalkSpeed.NORMAL, Accessibility.NEUTRAL);
+				LocationType.STATION, "8003781", null, "Lübeck Airport"), new Date(), true, Product.ALL, WalkSpeed.NORMAL, Accessibility.NEUTRAL);
 		System.out.println(result);
 		final QueryTripsResult laterResult = queryMoreTrips(result.context, true);
 		System.out.println(laterResult);
