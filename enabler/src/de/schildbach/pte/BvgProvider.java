a8efe538c146630ec88021fd39090ada79f0a90c
Andreas Schildbach <andreas@schildbach.de>
2014-08-13 22:50:21 +0200

    Fix umlauts in suggested locations for BVG.

--- enabler/src/de/schildbach/pte/BvgProvider.java
+++ enabler/src/de/schildbach/pte/BvgProvider.java
@@ -60,7 +60,7 @@ public final class BvgProvider extends AbstractHafasProvider
 
 	public BvgProvider(final String additionalQueryParameter)
 	{
-		super(API_BASE + "stboard.bin/dn", API_BASE + "ajax-getstop.bin/dny", API_BASE + "query.bin/dn", 8);
+		super(API_BASE + "stboard.bin/dn", API_BASE + "ajax-getstop.bin/dny", API_BASE + "query.bin/dn", 8, UTF_8);
 
 		setStyles(STYLES);
 
