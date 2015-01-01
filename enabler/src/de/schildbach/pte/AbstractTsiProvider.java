06a0d12b0f74fcfcee1ef68bdbf123c564b938eb
Andreas Schildbach <andreas@schildbach.de>
2015-01-01 01:52:27 +0100

    Update year of copyright.

--- enabler/src/de/schildbach/pte/AbstractTsiProvider.java
+++ enabler/src/de/schildbach/pte/AbstractTsiProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Kjell Braden <afflux@pentabarf.de>
+ * Copyright 2014-2015 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
