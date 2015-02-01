2d6de61d57c6a65d2e59f75b0df18cfcd7671534
Antonio El Khoury <antonio.elkhoury@gmail.com>
2015-02-01 18:43:43 +0100

    Paris: Use trip count attribute.

--- enabler/src/de/schildbach/pte/AbstractNavitiaProvider.java
+++ enabler/src/de/schildbach/pte/AbstractNavitiaProvider.java
@@ -1016,7 +1016,7 @@ public abstract class AbstractNavitiaProvider extends AbstractNetworkProvider
 				}
 
 				final StringBuilder queryUri = new StringBuilder(tripUri() + "journeys?" + "from=" + ParserUtils.urlEncode(fromString) + "&to="
-						+ ParserUtils.urlEncode(toString) + "&datetime=" + dateString + "&datetime_represents=" + dateTimeRep + "&count=1"
+						+ ParserUtils.urlEncode(toString) + "&datetime=" + dateString + "&datetime_represents=" + dateTimeRep + "&count=" + this.numTripsRequested
 						+ "&walking_speed=" + walkingSpeed + "&depth=0");
 
 				if (options != null && options.contains(Option.BIKE))
