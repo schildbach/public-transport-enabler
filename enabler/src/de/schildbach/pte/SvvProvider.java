015590866678c3580e9af0e831e921cf6bd7155b
Andreas Schildbach <andreas@schildbach.de>
2015-02-17 09:45:47 +0100

    Line styles for Salzburg.

--- enabler/src/de/schildbach/pte/SvvProvider.java
+++ enabler/src/de/schildbach/pte/SvvProvider.java
@@ -17,6 +17,11 @@
 
 package de.schildbach.pte;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import de.schildbach.pte.dto.Style;
+
 /**
  * @author Andreas Schildbach
  */
@@ -27,5 +32,30 @@ public class SvvProvider extends AbstractEfaProvider
 	public SvvProvider()
 	{
 		super(NetworkId.SVV, API_BASE);
+
+		setStyles(STYLES);
+	}
+
+	private static final Map<String, Style> STYLES = new HashMap<String, Style>();
+
+	static
+	{
+		STYLES.put("svv|SS1", new Style(Style.parseColor("#b61d33"), Style.WHITE));
+		STYLES.put("svv|SS2", new Style(Style.parseColor("#0069b4"), Style.WHITE));
+		STYLES.put("svv|SS3", new Style(Style.parseColor("#0aa537"), Style.WHITE));
+		STYLES.put("svv|SS4", new Style(Style.parseColor("#a862a4"), Style.WHITE));
+		STYLES.put("svv|SS11", new Style(Style.parseColor("#b61d33"), Style.WHITE));
+
+		STYLES.put("svv|B1", new Style(Style.parseColor("#e3000f"), Style.WHITE));
+		STYLES.put("svv|B2", new Style(Style.parseColor("#0069b4"), Style.WHITE));
+		STYLES.put("svv|B3", new Style(Style.parseColor("#956b27"), Style.WHITE));
+		STYLES.put("svv|B4", new Style(Style.parseColor("#ffcc00"), Style.WHITE));
+		STYLES.put("svv|B5", new Style(Style.parseColor("#04bbee"), Style.WHITE));
+		STYLES.put("svv|B6", new Style(Style.parseColor("#85bc22"), Style.WHITE));
+		STYLES.put("svv|B7", new Style(Style.parseColor("#009a9b"), Style.WHITE));
+		STYLES.put("svv|B8", new Style(Style.parseColor("#f39100"), Style.WHITE));
+		STYLES.put("svv|B10", new Style(Style.parseColor("#f8baa2"), Style.BLACK));
+		STYLES.put("svv|B12", new Style(Style.parseColor("#b9dfde"), Style.WHITE));
+		STYLES.put("svv|B14", new Style(Style.parseColor("#cfe09a"), Style.WHITE));
 	}
 }
