f7b0043e0a5c066f4d55ca1e00b760a376a2d467
Andreas Schildbach <andreas@schildbach.de>
2015-06-22 12:09:02 +0200

    Extract default line background colors to constants.

--- enabler/src/de/schildbach/pte/Standard.java
+++ enabler/src/de/schildbach/pte/Standard.java
@@ -29,17 +29,25 @@ import de.schildbach.pte.dto.Style.Shape;
  */
 public class Standard
 {
+	public static final int COLOR_BACKGROUND_HIGH_SPEED_TRAIN = Style.WHITE;
+	public static final int COLOR_BACKGROUND_REGIONAL_TRAIN = Style.GRAY;
+	public static final int COLOR_BACKGROUND_SUBURBAN_TRAIN = Style.parseColor("#006e34");
+	public static final int COLOR_BACKGROUND_SUBWAY = Style.parseColor("#003090");
+	public static final int COLOR_BACKGROUND_TRAM = Style.parseColor("#cc0000");
+	public static final int COLOR_BACKGROUND_BUS = Style.parseColor("#993399");
+	public static final int COLOR_BACKGROUND_FERRY = Style.BLUE;
+
 	public static final Map<Product, Style> STYLES = new HashMap<Product, Style>();
 
 	static
 	{
-		STYLES.put(Product.HIGH_SPEED_TRAIN, new Style(Shape.RECT, Style.WHITE, Style.RED, Style.RED));
-		STYLES.put(Product.REGIONAL_TRAIN, new Style(Shape.RECT, Style.GRAY, Style.WHITE));
-		STYLES.put(Product.SUBURBAN_TRAIN, new Style(Shape.CIRCLE, Style.parseColor("#006e34"), Style.WHITE));
-		STYLES.put(Product.SUBWAY, new Style(Shape.RECT, Style.parseColor("#003090"), Style.WHITE));
-		STYLES.put(Product.TRAM, new Style(Shape.RECT, Style.parseColor("#cc0000"), Style.WHITE));
-		STYLES.put(Product.BUS, new Style(Style.parseColor("#993399"), Style.WHITE));
-		STYLES.put(Product.FERRY, new Style(Shape.CIRCLE, Style.BLUE, Style.WHITE));
+		STYLES.put(Product.HIGH_SPEED_TRAIN, new Style(Shape.RECT, COLOR_BACKGROUND_HIGH_SPEED_TRAIN, Style.RED, Style.RED));
+		STYLES.put(Product.REGIONAL_TRAIN, new Style(Shape.RECT, COLOR_BACKGROUND_REGIONAL_TRAIN, Style.WHITE));
+		STYLES.put(Product.SUBURBAN_TRAIN, new Style(Shape.CIRCLE, COLOR_BACKGROUND_SUBURBAN_TRAIN, Style.WHITE));
+		STYLES.put(Product.SUBWAY, new Style(Shape.RECT, COLOR_BACKGROUND_SUBWAY, Style.WHITE));
+		STYLES.put(Product.TRAM, new Style(Shape.RECT, COLOR_BACKGROUND_TRAM, Style.WHITE));
+		STYLES.put(Product.BUS, new Style(COLOR_BACKGROUND_BUS, Style.WHITE));
+		STYLES.put(Product.FERRY, new Style(Shape.CIRCLE, COLOR_BACKGROUND_FERRY, Style.WHITE));
 		STYLES.put(null, new Style(Style.DKGRAY, Style.WHITE));
 	}
 }
