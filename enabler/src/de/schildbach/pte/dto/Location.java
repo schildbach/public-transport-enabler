0994d314433d55f483402609bca9b5451f3e958c
Andreas Schildbach <andreas@schildbach.de>
2015-09-20 22:55:24 +0200

    Add 'Schiffstation' to names that need places in their unique form.

--- enabler/src/de/schildbach/pte/dto/Location.java
+++ enabler/src/de/schildbach/pte/dto/Location.java
@@ -144,8 +144,9 @@ public final class Location implements Serializable
 		return false;
 	}
 
-	private static final String[] NON_UNIQUE_NAMES = { "Hauptbahnhof", "Hbf", "Bahnhof", "Bf", "Busbahnhof", "ZOB", "Zentrum", "Dorf", "Kirche",
-			"Nord", "Ost", "Süd", "West" };
+	private static final String[] NON_UNIQUE_NAMES = { "Hauptbahnhof", "Hbf", "Bahnhof", "Bf", "Busbahnhof", "ZOB", "Schiffstation", "Schiffst.",
+			"Zentrum", "Dorf", "Kirche", "Nord", "Ost", "Süd", "West" };
+
 	static
 	{
 		Arrays.sort(NON_UNIQUE_NAMES);
