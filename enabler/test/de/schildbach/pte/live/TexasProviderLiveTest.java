5563d636ff46830373630d4e482ee15995080ee4
Ojas Ahuja <OjasA@users.noreply.github.com>
2016-06-09 22:38:21 -0500

    Add Texas (United States) provider.

---
+++ enabler/test/de/schildbach/pte/live/TexasProviderLiveTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.schildbach.pte.live;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+import de.schildbach.pte.NetworkProvider.Accessibility;
+import de.schildbach.pte.NetworkProvider.WalkSpeed;
+import de.schildbach.pte.TexasProvider;
+import de.schildbach.pte.dto.Location;
+import de.schildbach.pte.dto.LocationType;
+import de.schildbach.pte.dto.NearbyLocationsResult;
+import de.schildbach.pte.dto.Product;
+import de.schildbach.pte.dto.QueryDeparturesResult;
+import de.schildbach.pte.dto.QueryTripsResult;
+import de.schildbach.pte.dto.SuggestLocationsResult;
+
+/**
+ * @author Ojas Ahuja
+ */
+public class TexasProviderLiveTest extends AbstractNavitiaProviderLiveTest
+{
+	public TexasProviderLiveTest()
+	{
+		super(new TexasProvider(secretProperty("navitia.authorization")));
+	}
+
+	@Test
+	public void nearbyStations() throws Exception
+	{
+		final NearbyLocationsResult result = queryNearbyStations(new Location(LocationType.STATION, "OCM:SP:CH506"));
+		print(result);
+	}
+
+	@Test
+	public void nearbyStationsByCoordinate() throws Exception
+	{
+		final NearbyLocationsResult result = queryNearbyStations(Location.coord(302747, -977404));
+		print(result);
+	}
+
+	@Test
+	public void queryDepartures() throws Exception
+	{
+		final QueryDeparturesResult result = queryDepartures("OCM:SP:CTP1179", false);
+		print(result);
+	}
+
+	@Test
+	public void queryDeparturesInvalidStation() throws Exception
+	{
+		final QueryDeparturesResult result = queryDepartures("OCM:SP:CTPxxxx", false);
+		assertEquals(QueryDeparturesResult.Status.INVALID_STATION, result.status);
+	}
+
+	@Test
+	public void suggestLocations() throws Exception
+	{
+		final SuggestLocationsResult result = suggestLocations("Airport");
+		print(result);
+	}
+}
