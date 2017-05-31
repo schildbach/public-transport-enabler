3bc1faf533a443c9938d6bd88279d5af2459256b
Andreas Schildbach <andreas@schildbach.de>
2017-05-31 10:04:07 +0200

    Migrate Norway to binary trips query.

--- enabler/src/de/schildbach/pte/NriProvider.java
+++ enabler/src/de/schildbach/pte/NriProvider.java
@@ -17,18 +17,11 @@
 
 package de.schildbach.pte;
 
-import java.io.IOException;
-import java.util.Date;
 import java.util.Set;
-
-import javax.annotation.Nullable;
 
 import com.google.common.base.Charsets;
 
-import de.schildbach.pte.dto.Location;
 import de.schildbach.pte.dto.Product;
-import de.schildbach.pte.dto.QueryTripsContext;
-import de.schildbach.pte.dto.QueryTripsResult;
 
 import okhttp3.HttpUrl;
 
@@ -60,19 +53,6 @@ public class NriProvider extends AbstractHafasLegacyProvider {
     @Override
     public Set<Product> defaultProducts() {
         return Product.ALL;
-    }
-
-    @Override
-    public QueryTripsResult queryTrips(final Location from, final @Nullable Location via, final Location to,
-            final Date date, final boolean dep, final @Nullable Set<Product> products,
-            final @Nullable Optimize optimize, final @Nullable WalkSpeed walkSpeed,
-            final @Nullable Accessibility accessibility, final @Nullable Set<Option> options) throws IOException {
-        return queryTripsXml(from, via, to, date, dep, products, walkSpeed, accessibility, options);
-    }
-
-    @Override
-    public QueryTripsResult queryMoreTrips(final QueryTripsContext context, final boolean later) throws IOException {
-        return queryMoreTripsXml(context, later);
     }
 
     @Override
