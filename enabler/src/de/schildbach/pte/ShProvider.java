3c961d86f68eae3c75fe8cf11bf9de9676531a05
Andreas Schildbach <andreas@schildbach.de>
2017-08-31 10:44:22 +0200

    SH: Update API base to HTTPS.

--- enabler/src/de/schildbach/pte/ShProvider.java
+++ enabler/src/de/schildbach/pte/ShProvider.java
@@ -35,7 +35,7 @@ import okhttp3.HttpUrl;
  * @author Andreas Schildbach
  */
 public class ShProvider extends AbstractHafasMobileProvider {
-    private static final HttpUrl API_BASE = HttpUrl.parse("http://nah.sh.hafas.de/bin/");
+    private static final HttpUrl API_BASE = HttpUrl.parse("https://nah.sh.hafas.de/bin/");
     private static final Product[] PRODUCTS_MAP = { Product.HIGH_SPEED_TRAIN, Product.HIGH_SPEED_TRAIN,
             Product.HIGH_SPEED_TRAIN, Product.REGIONAL_TRAIN, Product.SUBURBAN_TRAIN, Product.BUS, Product.FERRY,
             Product.SUBWAY, Product.TRAM, Product.ON_DEMAND };
