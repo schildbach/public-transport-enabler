6a4792e879429cf20d7b1da9dda5b094e855cee0
Andreas Schildbach <andreas@schildbach.de>
2017-08-31 10:30:15 +0200

    BVG: Update API base to HTTPS.

--- enabler/src/de/schildbach/pte/BvgProvider.java
+++ enabler/src/de/schildbach/pte/BvgProvider.java
@@ -39,7 +39,7 @@ import okhttp3.HttpUrl;
  * @author Andreas Schildbach
  */
 public final class BvgProvider extends AbstractHafasMobileProvider {
-    private static final HttpUrl API_BASE = HttpUrl.parse("http://bvg-apps.hafas.de/bin/");
+    private static final HttpUrl API_BASE = HttpUrl.parse("https://bvg-apps.hafas.de/bin/");
     private static final Product[] PRODUCTS_MAP = { Product.SUBURBAN_TRAIN, Product.SUBWAY, Product.TRAM, Product.BUS,
             Product.FERRY, Product.HIGH_SPEED_TRAIN, Product.REGIONAL_TRAIN, Product.ON_DEMAND, null, null };
 
