b2875ddd31672fef47d9355e5ab5ac4c4ef527df
Andreas Schildbach <andreas@schildbach.de>
2018-07-15 16:18:10 +0200

    Update links in license to use HTTPS.

--- enabler/src/de/schildbach/pte/util/LittleEndianDataInputStream.java
+++ enabler/src/de/schildbach/pte/util/LittleEndianDataInputStream.java
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If notf, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If notf, see <https://www.gnu.org/licenses/>.
  */
 
 package de.schildbach.pte.util;
