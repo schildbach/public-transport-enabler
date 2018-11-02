a8ef6c365a76a704223e70ff74e8becddcf704f4
Andreas Schildbach <andreas@schildbach.de>
2018-11-02 13:20:17 +0100

    DSB: 'Pågatog' line.

--- enabler/src/de/schildbach/pte/DsbProvider.java
+++ enabler/src/de/schildbach/pte/DsbProvider.java
@@ -77,6 +77,8 @@ public class DsbProvider extends AbstractHafasLegacyProvider {
             return Product.REGIONAL_TRAIN;
         if ("L".equals(ucType)) // Lokalbane
             return Product.REGIONAL_TRAIN;
+        if ("PÅGATOG".equals(ucType))
+            return Product.REGIONAL_TRAIN;
 
         if ("S-TOG".equals(ucType))
             return Product.SUBURBAN_TRAIN;
