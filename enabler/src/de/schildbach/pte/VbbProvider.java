760d373065c0e66584bf4f0a8b2395cf6e8d7287
Andreas Schildbach <andreas@schildbach.de>
2019-01-05 16:56:50 +0100

    VBB: Sync to 4.2.4.

--- enabler/src/de/schildbach/pte/VbbProvider.java
+++ enabler/src/de/schildbach/pte/VbbProvider.java
@@ -40,7 +40,8 @@ public class VbbProvider extends AbstractHafasClientInterfaceProvider {
 
     public VbbProvider(final String apiAuthorization, final byte[] salt) {
         super(NetworkId.VBB, API_BASE, PRODUCTS_MAP);
-        setApiVersion("1.14");
+        setApiVersion("1.16");
+        setApiExt("VBB.4");
         setApiClient("{\"id\":\"VBB\",\"type\":\"AND\"}");
         setApiAuthorization(apiAuthorization);
         setRequestMicMacSalt(salt);
