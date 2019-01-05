a29613c0c4b0341e9f4e4351cc5ecf72357b8669
Andreas Schildbach <andreas@schildbach.de>
2019-01-05 17:14:29 +0100

    LU: Sync to 3.5.5.

--- enabler/src/de/schildbach/pte/LuProvider.java
+++ enabler/src/de/schildbach/pte/LuProvider.java
@@ -36,7 +36,7 @@ public class LuProvider extends AbstractHafasClientInterfaceProvider {
 
     public LuProvider(final String apiAuthorization) {
         super(NetworkId.LU, API_BASE, PRODUCTS_MAP);
-        setApiVersion("1.16");
+        setApiVersion("1.21");
         setApiClient("{\"id\":\"CDT\",\"type\":\"AND\"}");
         setApiAuthorization(apiAuthorization);
     }
