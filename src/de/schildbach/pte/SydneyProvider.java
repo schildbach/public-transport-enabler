2afb06e326bf7b6454a60b5c9cc25a8601834d0d
Dongchen Yue <yuedongchenyy@gmail.com>
2021-01-18 18:22:44 +0100

    Sydney: 'M' line.

--- src/de/schildbach/pte/SydneyProvider.java
+++ src/de/schildbach/pte/SydneyProvider.java
@@ -126,6 +126,13 @@ public class SydneyProvider extends AbstractEfaProvider {
             throw new IllegalStateException("cannot normalize mot='" + mot + "' symbol='" + symbol + "' name='" + name
                     + "' long='" + longName + "' trainType='" + trainType + "' trainNum='" + trainNum + "' trainName='"
                     + trainName + "'");
+        } else if ("2".equals(mot)) {
+            if ("M".equals(symbol) || "M Metro North West Line".equals(symbol))
+                return new Line(id, network, Product.SUBWAY, "M");
+
+            throw new IllegalStateException("cannot normalize mot='" + mot + "' symbol='" + symbol + "' name='" + name
+                    + "' long='" + longName + "' trainType='" + trainType + "' trainNum='" + trainNum + "' trainName='"
+                    + trainName + "'");
         } else if ("4".equals(mot)) {
             if ("L1".equals(symbol) || "L1 Dulwich Hill Line".equals(symbol))
                 return new Line(id, network, Product.TRAM, "L1");
