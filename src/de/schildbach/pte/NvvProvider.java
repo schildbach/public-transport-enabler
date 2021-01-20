e9723c5aa73a2e366c6ed8bef517df955bd88295
Dominic Schmidt <dominic.s.hanau@gmail.com>
2021-01-21 00:59:16 +0100

    NVV: Add styles for Frankfurt.

--- src/de/schildbach/pte/NvvProvider.java
+++ src/de/schildbach/pte/NvvProvider.java
@@ -17,11 +17,13 @@
 
 package de.schildbach.pte;
 
-import java.util.regex.Matcher;
-
 import de.schildbach.pte.dto.Product;
-
+import de.schildbach.pte.dto.Style;
 import okhttp3.HttpUrl;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
 
 /**
  * Provider implementation for the Nordhessischer Verkehrsverbund (North Hesse, Germany).
@@ -45,6 +47,7 @@ public class NvvProvider extends AbstractHafasClientInterfaceProvider {
         setApiExt("NVV.6.0");
         setApiClient(apiClient);
         setApiAuthorization(apiAuthorization);
+        setStyles(STYLES);
     }
 
     private static final String[] PLACES = { "Frankfurt (Main)", "Offenbach (Main)", "Mainz", "Wiesbaden", "Marburg",
@@ -75,5 +78,31 @@ public class NvvProvider extends AbstractHafasClientInterfaceProvider {
         if (m.matches())
             return new String[] { m.group(1), m.group(2) };
         return super.splitStationName(address);
+    }
+
+    private static final Map<String, Style> STYLES = new HashMap<>();
+
+    static {
+        STYLES.put("SS1", new Style(Style.parseColor("#009edd"), Style.WHITE));
+        STYLES.put("SS2", new Style(Style.parseColor("#ff2e17"), Style.WHITE));
+        STYLES.put("SS3", new Style(Style.parseColor("#00b098"), Style.WHITE));
+        STYLES.put("SS4", new Style(Style.parseColor("#ffc734"), Style.parseColor("#2c2e35"), Style.parseColor(
+                "#2c2e35")));
+        STYLES.put("SS5", new Style(Style.parseColor("#95542a"), Style.WHITE));
+        STYLES.put("SS6", new Style(Style.parseColor("#ff7322"), Style.WHITE));
+        STYLES.put("SS7", new Style(Style.parseColor("#214d36"), Style.WHITE));
+        STYLES.put("SS8", new Style(Style.parseColor("#88c946"), Style.WHITE));
+        STYLES.put("SS9", new Style(Style.parseColor("#872996"), Style.WHITE));
+
+        STYLES.put("UU1", new Style(Style.parseColor("#c52b1e"), Style.WHITE));
+        STYLES.put("UU2", new Style(Style.parseColor("#00ab4f"), Style.WHITE));
+        STYLES.put("UU3", new Style(Style.parseColor("#345aaf"), Style.WHITE));
+        STYLES.put("UU4", new Style(Style.parseColor("#fc5cac"), Style.WHITE));
+        STYLES.put("UU5", new Style(Style.parseColor("#0c7d3e"), Style.WHITE));
+        STYLES.put("UU6", new Style(Style.parseColor("#0082ca"), Style.WHITE));
+        STYLES.put("UU7", new Style(Style.parseColor("#f19e2d"), Style.WHITE));
+        STYLES.put("UU8", new Style(Style.parseColor("#ca7fbe"), Style.WHITE));
+        STYLES.put("UU9", new Style(Style.parseColor("#f4d039"), Style.parseColor("#2c2e35"), Style.parseColor(
+                "#2c2e35")));
     }
 }
