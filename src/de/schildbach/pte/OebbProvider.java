224cd244a37b520c2774ac6d853e27653d6b86bd
Andreas Schildbach <andreas@schildbach.de>
2025-12-02 10:00:20 +0100

    OEBB: update API base and endpoint

--- src/de/schildbach/pte/OebbProvider.java
+++ src/de/schildbach/pte/OebbProvider.java
@@ -30,7 +30,7 @@ import okhttp3.HttpUrl;
  * @author Andreas Schildbach
  */
 public class OebbProvider extends AbstractHafasClientInterfaceProvider {
-    private static final HttpUrl API_BASE = HttpUrl.parse("https://fahrplan.oebb.at/bin/");
+    private static final HttpUrl API_BASE = HttpUrl.parse("https://fahrplan.oebb.at/");
     private static final Product[] PRODUCTS_MAP = { Product.HIGH_SPEED_TRAIN, Product.HIGH_SPEED_TRAIN,
             Product.HIGH_SPEED_TRAIN, Product.REGIONAL_TRAIN, Product.REGIONAL_TRAIN, Product.SUBURBAN_TRAIN,
             Product.BUS, Product.FERRY, Product.SUBWAY, Product.TRAM, Product.HIGH_SPEED_TRAIN, Product.ON_DEMAND,
@@ -47,6 +47,7 @@ public class OebbProvider extends AbstractHafasClientInterfaceProvider {
 
     public OebbProvider(final HttpUrl apiBase, final String apiClient, final String apiAuthorization) {
         super(NetworkId.OEBB, apiBase, PRODUCTS_MAP);
+        setApiEndpoint("gate");
         setApiVersion("1.39");
         setApiClient(apiClient);
         setApiAuthorization(apiAuthorization);
