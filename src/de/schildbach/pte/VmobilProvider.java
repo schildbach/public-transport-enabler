3c2200e0a0e721815296bdc7cb18a82ed9bf461b
Andreas Schildbach <andreas@schildbach.de>
2026-01-02 19:56:01 +0100

    VMOBIL: update API ext

--- src/de/schildbach/pte/VmobilProvider.java
+++ src/de/schildbach/pte/VmobilProvider.java
@@ -45,7 +45,7 @@ public class VmobilProvider extends AbstractHafasClientInterfaceProvider {
         super(NetworkId.VMOBIL, API_BASE, PRODUCTS_MAP);
         setApiEndpoint("gate");
         setApiVersion("1.59");
-        setApiExt("VAO.6");
+        setApiExt("VAO.22");
         setApiClient(apiClient);
         setApiAuthorization(apiAuthorization);
     }
