48b63e56892ce456d9482f5826473f1d18cbbd7c
Andreas Schildbach <andreas@schildbach.de>
2026-01-02 19:59:39 +0100

    VOR: update API base and endpoint

--- src/de/schildbach/pte/VorProvider.java
+++ src/de/schildbach/pte/VorProvider.java
@@ -34,7 +34,7 @@ import okhttp3.HttpUrl;
  * @author Andreas Schildbach
  */
 public class VorProvider extends AbstractHafasClientInterfaceProvider {
-    private static final HttpUrl API_BASE = HttpUrl.parse("https://anachb.vor.at/bin/");
+    private static final HttpUrl API_BASE = HttpUrl.parse("https://anachb.vor.at/hamm/");
     private static final Product[] PRODUCTS_MAP = { Product.HIGH_SPEED_TRAIN, Product.SUBURBAN_TRAIN, Product.SUBWAY,
             null, Product.TRAM, Product.REGIONAL_TRAIN, Product.BUS, Product.BUS, Product.TRAM, Product.FERRY,
             Product.ON_DEMAND, Product.BUS, Product.REGIONAL_TRAIN, null, null, null };
@@ -46,6 +46,7 @@ public class VorProvider extends AbstractHafasClientInterfaceProvider {
 
     public VorProvider(final String apiClient, final String apiAuthorization) {
         super(NetworkId.VOR, API_BASE, PRODUCTS_MAP);
+        setApiEndpoint("gate");
         setApiVersion("1.59");
         setApiExt("VAO.9");
         setApiClient(apiClient);
